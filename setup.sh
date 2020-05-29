7578cbb4668bcab78ab7e8e1b311f4c36aa99348
pitprok <44905554+pitprok@users.noreply.github.com>
2020-05-29 11:28:38 +0300

    Removed unused files and added proxy-balancer-configmap to setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ popd
 kubectl apply -f service-account.yaml
 kubectl apply -f pod-modifier-role.yaml
 kubectl apply -f pod-modifier-binding.yaml
-kubectl apply -f httpd-config-map.yaml
+kubectl apply -f httpd-configmap.yaml
+kubectl apply -f proxy-balancer-configmap.yaml
 kubectl apply -f httpd.yaml
 kubectl apply -f tomcat-detector.yaml
