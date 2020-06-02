222ea61bd32d461a3d6b5ec07831c59940ad5116
pitprok <44905554+pitprok@users.noreply.github.com>
2020-06-02 19:41:01 +0300

    Code refactoring in tomcat-detector.py

--- tomcat-detector/tomcat_detector.py
+++ tomcat-detector/tomcat_detector.py
@@ -22,20 +22,17 @@ def reload_httpd():
     httpd_reload_command = "kubectl,exec,httpd,--container,"+httpd_container_name+",--,"+httpd_binary+",-k,graceful"
     command_array = httpd_reload_command.split(",")
     subprocess.check_call(command_array, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
-    print("")
 
 
 def add_to_balancer(pod_ip, container_port):
-    sed_expression = "s|\\(<Proxy \"balancer:.*>\\)|\\1\\n    BalancerMember \"http://" + pod_ip + ":"+ container_port +"\"|"
-    add_balancer_member_command = [
-        "kubectl", "exec", httpd_pod_name, "--", "sed", "-i", sed_expression, proxy_balancer_conf]
+    sed_expression = "s|\\(<Proxy \"balancer:.*>\\)|\\1\\n    BalancerMember \"http://"+pod_ip+":"+container_port+"\"|"
+    add_balancer_member_command = ["kubectl", "exec", httpd_pod_name, "--", "sed", "-i", sed_expression, proxy_balancer_conf]
     subprocess.check_call(add_balancer_member_command)
 
 
 def remove_from_balancer(pod_ip, container_port):
-    sed_expression = "/    BalancerMember \"http:\/\/" + pod_ip + ":" + container_port + "\"/d"
-    remove_balancer_member_command = [
-        "kubectl", "exec", httpd_pod_name, "--", "sed", "-i", sed_expression, proxy_balancer_conf]
+    sed_expression = "/    BalancerMember \"http:\/\/"+pod_ip+":"+container_port+"\"/d"
+    remove_balancer_member_command = ["kubectl", "exec", httpd_pod_name, "--", "sed", "-i", sed_expression, proxy_balancer_conf]
     subprocess.check_call(remove_balancer_member_command)
 
 
@@ -91,6 +88,59 @@ def ip_already_exists(pod_ip, container_port):
         else:
             raise
 
+def handle_tomcat_pod(pod,tomcats):
+    pod_ip = get_pod_IP(pod)
+    pod_name = pod.metadata.name
+    ### Empty line to separate log events ###
+    print()
+    print("Namespace: %s Pod Name: %s" %
+                (pod.metadata.namespace, pod.metadata.name))
+    if pod_ip is not None:
+        if pod.status.conditions is not None and \
+            pod.status.container_statuses is not None:
+
+            tomcat_status = get_tomcat_container_status(pod)
+            tomcat_specs = get_tomcat_container_specs(pod.spec.containers, tomcat_status)
+            container_active = container_is_active(pod, tomcat_status)
+            ## We assume that only one port is exposed on the tomcat container ###
+            container_port = str(tomcat_specs.ports[0].container_port)
+
+            if pod_ip in tomcats and not container_active:
+
+                print("Tomcat server in pod \""+pod_name +"\" went offline/crashed, removing it from proxy_balancer.conf")
+                remove_from_balancer(pod_ip, container_port)
+                tomcats.remove(pod_ip)
+                reload_httpd()
+
+            elif pod_ip not in tomcats and container_active:
+
+                if tomcat_responds(pod_name, pod_ip, container_port):
+                    if ip_already_exists(pod_ip, container_port):
+                        print("IP of pod \"" + pod_name + "\" already in proxy_balancer.conf. Skipping...")
+                        tomcats.append(pod_ip)
+                    else:
+                        print("Tomcat server in pod \""+pod_name +"\" is online, adding it to proxy_balancer.conf")
+                        add_to_balancer(pod_ip, container_port)
+                        tomcats.append(pod_ip)
+                        reload_httpd()
+
+                else:
+                    print("Can't get a response from tomcat, check pod "+pod_name)
+
+            ### Checks if tomcat is in the process of being deleted to prevent unnecessary error messages ###
+            elif pod_ip not in tomcats and \
+                (pod.metadata.deletion_grace_period_seconds is not None or
+                pod.metadata.deletion_timestamp is not None):
+
+                print("Pod \""+pod_name+"\" is being deleted.")
+
+            else:
+                print("Tomcat on pod \""+pod_name+"\" is offline.")
+        else:
+            print("Pod status elements for pod \"" +pod_name+"\" are not initialized yet.")
+    else:
+        print("Pod \""+pod_name+"\" has no IP yet.")
+
 def main():
     config.load_incluster_config()
     tomcats = []
@@ -102,58 +152,9 @@ def main():
         #TODO lookout for httpd delete events
         #TODO handle no-existent httpd
         pod = event['object']
-        pod_ip = get_pod_IP(pod)
-        pod_name = pod.metadata.name
-        print("Namespace: %s Pod Name: %s" %
-                    (pod.metadata.namespace, pod.metadata.name))
+
         if pod_contains_tomcat(pod):
-
-            if pod_ip is not None:
-                if pod.status.conditions is not None and \
-                    pod.status.container_statuses is not None:
-
-                    tomcat_status = get_tomcat_container_status(pod)
-                    tomcat_specs = get_tomcat_container_specs(pod.spec.containers, tomcat_status)
-                    container_active = container_is_active(pod, tomcat_status)
-                    ## We assume that only one port is exposed on the tomcat container ###
-                    container_port = str(tomcat_specs.ports[0].container_port)
-
-                    if pod_ip in tomcats and not container_active:
-
-                        print("Tomcat server in pod \""+pod_name +"\" went offline/crashed, removing it from proxy_balancer.conf")
-                        remove_from_balancer(pod_ip, container_port)
-                        tomcats.remove(pod_ip)
-                        reload_httpd()
-
-                    elif pod_ip not in tomcats and container_active:
-
-                        if tomcat_responds(pod_name, pod_ip, container_port):
-                            if ip_already_exists(pod_ip, container_port):
-                                print("IP of pod \"" + pod_name + "\" already in proxy_balancer.conf. Skipping...")
-                                print("")
-                                tomcats.append(pod_ip)
-                            else:
-                                print("Tomcat server in pod \""+pod_name +"\" is online, adding it to proxy_balancer.conf")
-                                add_to_balancer(pod_ip, container_port)
-                                tomcats.append(pod_ip)
-                                reload_httpd()
-
-                        else:
-                            print("Can't get a response from tomcat, check pod "+pod_name)
-
-                    ### Checks if tomcat is in the process of being deleted to prevent unnecessary error messages ###
-                    elif pod_ip not in tomcats and \
-                        (pod.metadata.deletion_grace_period_seconds is not None or
-                        pod.metadata.deletion_timestamp is not None):
-
-                        pass
-
-                    else:
-                        print("Tomcat on pod \""+pod_name+"\" is offline.")
-                else:
-                    print("Pod status elements for pod \"" +pod_name+"\" are not initialized yet.")
-            else:
-                print("Pod \""+pod_name+"\" has no IP yet.")
+            handle_tomcat_pod(pod,tomcats)
 
 
 
